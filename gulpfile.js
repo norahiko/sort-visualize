f32dfc36e3cfc7d3578a68395ebbc8eb17d2216c
norahiko <norahiko.o@gmail.com>
2014-02-24 01:37:22 +0900

    Add stylus environment

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,35 @@
+"use strict";
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var stylus = require('gulp-stylus');
 
 var srcFiles = 'src/**/*.js';
 var destSrc = 'app.js';
 var destDir = 'app/js';
 
-gulp.task('default', ['build']);
+var stylusFiles = 'app/css/*.styl';
+var stylusDestDir = 'app/css';
+
+
+gulp.task('default', ['build', 'stylus']);
 
 gulp.task('watch', function() {
-    gulp.watch(srcFiles, ['build']);
+    gulp.watch(srcFiles, ['js']);
+    gulp.watch(stylusFiles, ['stylus']);
 });
 
-gulp.task('build', function() {
+gulp.task('build', ['js', 'stylus']);
+
+gulp.task('js', function() {
     return gulp.src(srcFiles)
                .pipe(concat(destSrc))
                .pipe(gulp.dest(destDir));
 });
+
+gulp.task('stylus', function() {
+    return gulp.src(stylusFiles)
+               .pipe(stylus())
+               .pipe(gulp.dest(stylusDestDir));
+});
+
